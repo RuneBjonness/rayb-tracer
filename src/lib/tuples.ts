fd8a13ec543f167e426747a79a71f6e6eb90ca37
Rune Bjønness <rune.bjonness@gmail.com>
2022-11-27 00:21:26 +0100

    Optimize tuple operations

--- src/lib/tuples.ts
+++ src/lib/tuples.ts
@@ -44,19 +44,19 @@ export function areEqual(t1: Tuple | Color, t2: Tuple | Color): boolean {
 }
 
 export function add(t1: Tuple, t2: Tuple): Tuple {
-  return t1.map((v, i) => v + t2[i]) as Tuple;
+  return [t1[0] + t2[0], t1[1] + t2[1], t1[2] + t2[2], t1[3] + t2[3]];
 }
 
 export function addColors(c1: Color, c2: Color): Color {
-  return c1.map((v, i) => v + c2[i]) as Color;
+  return [c1[0] + c2[0], c1[1] + c2[1], c1[2] + c2[2]];
 }
 
 export function subtract(t1: Tuple, t2: Tuple): Tuple {
-  return t1.map((v, i) => v - t2[i]) as Tuple;
+  return [t1[0] - t2[0], t1[1] - t2[1], t1[2] - t2[2], t1[3] - t2[3]];
 }
 
 export function subtractColors(c1: Color, c2: Color): Color {
-  return c1.map((v, i) => v - c2[i]) as Color;
+  return [c1[0] - c2[0], c1[1] - c2[1], c1[2] - c2[2]];
 }
 
 export function negate(t: Tuple): Tuple {
@@ -65,37 +65,36 @@ export function negate(t: Tuple): Tuple {
 }
 
 export function multiplyTupleByScalar(t: Tuple, scalar: number): Tuple {
-  return t.map((v) => v * scalar) as Tuple;
+  return [t[0] * scalar, t[1] * scalar, t[2] * scalar, t[3] * scalar];
 }
 
 export function multiplyTuples(a: Tuple, b: Tuple): Tuple {
-  return a.map((v, i) => v * b[i]) as Tuple;
+  return [a[0] * b[0], a[1] * b[1], a[2] * b[2], a[3] * b[3]];
 }
 
 export function multiplyColorByScalar(c: Color, scalar: number): Color {
-  return c.map((v) => v * scalar) as Color;
+  return [c[0] * scalar, c[1] * scalar, c[2] * scalar];
 }
 
 export function multiplyColors(a: Color, b: Color): Color {
-  return a.map((v, i) => v * b[i]) as Color;
+  return [a[0] * b[0], a[1] * b[1], a[2] * b[2]];
 }
 
 export function divide(t: Tuple, scalar: number): Tuple {
-  return t.map((v) => v / scalar) as Tuple;
+  return [t[0] / scalar, t[1] / scalar, t[2] / scalar, t[3] / scalar];
 }
 
 export function divideColor(c: Color, scalar: number): Color {
-  return c.map((v) => v / scalar) as Color;
+  return [c[0] / scalar, c[1] / scalar, c[2] / scalar];
 }
 
 export function magnitude(t: Tuple): number {
-  const [x, y, z, w] = t;
-  return Math.sqrt(x ** 2 + y ** 2 + z ** 2 + w ** 2);
+  return Math.sqrt(t[0] ** 2 + t[1] ** 2 + t[2] ** 2 + t[3] ** 2);
 }
 
 export function normalize(t: Tuple): Tuple {
   const m = magnitude(t);
-  return t.map((v) => v / m) as Tuple;
+  return [t[0] / m, t[1] / m, t[2] / m, t[3] / m];
 }
 
 export function dot(t1: Tuple, t2: Tuple): number {
