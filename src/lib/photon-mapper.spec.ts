f38d32fb755e421b630d0c22467349f4c8575216
Rune Bjønness <rune.bjonness@gmail.com>
2022-12-28 14:55:33 +0100

    Add calculatePhotonMaps function

--- src/lib/photon-mapper.spec.ts
+++ src/lib/photon-mapper.spec.ts
@@ -1,6 +1,8 @@
 import { AreaLight, PointLight } from './lights';
 import { PhotonMapper } from './photon-mapper';
-import { point, vector, color } from './tuples';
+import { Sphere } from './shapes/primitives/sphere';
+import { scaling } from './transformations';
+import { point, color } from './tuples';
 import { World } from './world';
 
 describe('photon-mapping', () => {
@@ -31,5 +33,58 @@ describe('photon-mapping', () => {
 
     expect(photonMapper.totalLightIntensity).toBeCloseTo(3);
     expect(photonMapper.photonPowerFactor).toBeCloseTo(0.003);
+  });
+
+  test('a photon mapper calculates global illumination by tracing photons and storing diffuse reflections', () => {
+    const w = new World();
+    w.lights.push(new PointLight(point(0, 0, 0), color(1, 1, 1)));
+
+    const s = new Sphere();
+    s.material.diffuse = 1;
+    w.objects.push(s);
+
+    const photonMapper = new PhotonMapper(w, 10, 2);
+    const result = photonMapper.calculatePhotonMaps(10);
+
+    expect(result.globalMap.length).toBeGreaterThan(10);
+    expect(result.causticMap.length).toEqual(0);
+  });
+
+  test('a photon mapper stores diffuse reflections from photons that have been through at least one specular reflection in a caustic map', () => {
+    const w = new World();
+    w.lights.push(new PointLight(point(0, 0, 0), color(1, 1, 1)));
+
+    const s = new Sphere();
+    s.material.diffuse = 0.5;
+    s.material.reflective = 0.5;
+    w.objects.push(s);
+
+    const photonMapper = new PhotonMapper(w, 50, 4);
+    const result = photonMapper.calculatePhotonMaps(50);
+
+    expect(result.causticMap.length).toBeGreaterThan(0);
+  });
+
+  test('the power of a photon reflected from a surface or transmitted through a transparent object will get blended with the color of the material', () => {
+    const w = new World();
+    w.lights.push(new PointLight(point(0, 0, 0), color(1, 0, 0)));
+
+    const g = new Sphere();
+    g.material.diffuse = 0;
+    g.material.transparancy = 1;
+    g.material.color = color(0, 0, 1);
+
+    const s = new Sphere();
+    s.material.diffuse = 1;
+    s.material.transparancy = 0;
+    s.transform = scaling(2, 2, 2);
+
+    w.objects.push(g, s);
+
+    const photonMapper = new PhotonMapper(w, 50, 4);
+    const result = photonMapper.calculatePhotonMaps(50);
+
+    expect(result.causticMap.length).toBeGreaterThan(0);
+    expect(result.causticMap[0].power[2]).toBeCloseTo(0.5);
   });
 });
--- src/lib/photon-mapper.ts
+++ src/lib/photon-mapper.ts
@@ -1,4 +1,12 @@
-import { addColors, Color, Tuple } from './tuples';
+import {
+  hit,
+  prepareComputations,
+  reflectance,
+  refractedDirection,
+} from './intersections';
+import { materialColorAt } from './materials';
+import { ray } from './rays';
+import { addColors, blendColors, Color, Tuple, vector } from './tuples';
 import { World } from './world';
 
 export type Photon = {
@@ -6,6 +14,11 @@ export type Photon = {
   direction: Tuple;
   power: Color;
   interactedWithSpecular: boolean;
+};
+
+export type PhotonMapResult = {
+  globalMap: Photon[];
+  causticMap: Photon[];
 };
 
 export class PhotonMapper {
@@ -25,7 +38,134 @@ export class PhotonMapper {
     this.photonPowerFactor = this.totalLightIntensity / totalPhotons;
   }
 
-  trace(count: number): Photon[] {
+  calculatePhotonMaps(photonCount: number): PhotonMapResult {
+    const result: PhotonMapResult = {
+      globalMap: [],
+      causticMap: [],
+    };
+
+    const photons = this.emitPhotons(photonCount);
+
+    for (let i = 0; i < photonCount; i++) {
+      this.trace(photons[i], result.globalMap, result.causticMap);
+    }
+
+    return result;
+  }
+
+  private trace(
+    photon: Photon,
+    globalMap: Photon[],
+    causticMap: Photon[],
+    bounces = 0
+  ): void {
+    if (bounces > this.maxBounces) {
+      return;
+    }
+
+    const r = ray(photon.position, photon.direction);
+    const xs = this.world.intersects(r);
+    const i = hit(xs);
+
+    if (i === null) {
+      return;
+    }
+    const ic = prepareComputations(i, r, xs);
+
+    const specularReflection = Math.max(
+      i.object.material.reflective,
+      i.object.material.transparancy
+    );
+    const diffuseReflection = Math.min(
+      i.object.material.diffuse,
+      1 - specularReflection
+    );
+
+    const interactionResult = Math.random();
+    if (interactionResult < diffuseReflection) {
+      if (bounces > 0) {
+        const interaction: Photon = {
+          position: ic.point,
+          direction: photon.direction,
+          power: photon.power,
+          interactedWithSpecular: photon.interactedWithSpecular,
+        };
+        if (photon.interactedWithSpecular) {
+          causticMap.push(interaction);
+        } else {
+          globalMap.push(interaction);
+        }
+      }
+
+      this.trace(
+        {
+          position: ic.point,
+          direction: vector(
+            ic.normalv[0] * Math.random(),
+            ic.normalv[1] * Math.random(),
+            ic.normalv[2] * Math.random()
+          ),
+          power: blendColors(
+            photon.power,
+            materialColorAt(ic.object, ic.point)
+          ),
+          interactedWithSpecular: photon.interactedWithSpecular,
+        },
+        globalMap,
+        causticMap,
+        bounces + 1
+      );
+    } else if (interactionResult - diffuseReflection < specularReflection) {
+      let reflect: boolean;
+
+      if (
+        ic.object.material.reflective > 0 &&
+        ic.object.material.transparancy > 0
+      ) {
+        reflect =
+          interactionResult - diffuseReflection <
+          specularReflection * reflectance(ic);
+      } else {
+        reflect = ic.object.material.reflective > 0;
+      }
+      if (reflect) {
+        this.trace(
+          {
+            position: ic.overPoint,
+            direction: ic.reflectv,
+            power: blendColors(
+              photon.power,
+              materialColorAt(ic.object, ic.point)
+            ),
+            interactedWithSpecular: true,
+          },
+          globalMap,
+          causticMap,
+          bounces + 1
+        );
+      } else {
+        const dir = refractedDirection(ic);
+        if (dir !== null) {
+          this.trace(
+            {
+              position: ic.underPoint,
+              direction: dir,
+              power: blendColors(
+                photon.power,
+                materialColorAt(ic.object, ic.point)
+              ),
+              interactedWithSpecular: true,
+            },
+            globalMap,
+            causticMap,
+            bounces + 1
+          );
+        }
+      }
+    }
+  }
+
+  private emitPhotons(count: number): Photon[] {
     const photons: Photon[] = [];
     this.world.lights.forEach((l) => {
       const emitCount =
