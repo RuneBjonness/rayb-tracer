9da418a80db3378f9bcb820bb92abb16fbcb834c
Rune Bjønness <rune.bjonness@gmail.com>
2024-01-20 12:04:37 +0100

    Configure deploy base to support github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    assetsInclude: ['obj', 'ppm'],
+  plugins: [react()],
+  assetsInclude: ['obj', 'ppm'],
+  base: '/rayb-tracer/',
 });
